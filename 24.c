e33c707c095878cac23fdb259ee2f03baaef23e3
Karim Almasry <47185587+karimsayedalmasry@users.noreply.github.com>
2020-12-03 08:52:51 +0200

    updates

---
+++ 24.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int bin(int x) ; 
+void main(void)
+{
+int x ; 
+scanf("%d" , &x);
+ bin(x);
+}
+
+int bin(int x)
+{
+    while(x>0){
+printf("%d",x%2);  
+x=x/2; 
+ return bin(x);
+}}
