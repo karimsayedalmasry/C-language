c26f23dee200296211eeed2702650b80c4c96460
Karim Almasry <47185587+karimsayedalmasry@users.noreply.github.com>
2020-12-07 02:39:22 +0200

    updates

---
+++ 35.c
@@ -0,0 +1,30 @@
+#include<stdio.h> 
+
+void main (void) 
+{
+int arr[10] = {1,1,1,2,2,4,4,5,7,7};
+int occ[10] ={0}  ; 
+int temp ; 
+for(int i = 0 ; i<10 ; i++)
+{
+    temp = arr[i] ; 
+    for(int j = 0 ; j<10 ; j++)
+    {
+        if(temp == arr[j])
+        {
+            occ[i]++;
+
+        }
+    }
+}
+
+for(int j = 0 ; j<10 ; j++)
+    {
+        printf("%d," ,arr[j]);
+    }
+ printf("\n");
+for(int j = 0 ; j<10 ; j++)
+    {
+        printf("%d,", occ[j]);
+    }
+}
