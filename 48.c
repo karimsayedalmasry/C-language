4861cd2bd66dadaac8ff522944025dace711e2ec
Karim Almasry <47185587+karimsayedalmasry@users.noreply.github.com>
2020-12-10 09:20:34 +0200

    updates

--- 48.c
+++ 48.c
@@ -3,13 +3,20 @@
 void main(void)
 {
 char str[]  = {"karim"};
-char str1[]  ;
+char str1[5]  ;
+
+int x = 0 ; 
+
 for(int i = 4 ; i>=0 ; i--)
 {
-    str1[i] = str[i];
+    str1[x] = str[i];
+    x++;
 }
 
-
+for(int i = 0; i<5 ; i++)
+{
+   printf("%c,",  str1[i] );
+}
 
 
 }
